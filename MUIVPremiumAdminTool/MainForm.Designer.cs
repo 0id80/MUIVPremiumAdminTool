614f0e43791d00ae8022629a30d21531078b1473
0id80 <zekagod@gmail.com>
2022-05-29 23:39:12 +0300

    Add dark

--- MUIVPremiumAdminTool/MainForm.Designer.cs
+++ MUIVPremiumAdminTool/MainForm.Designer.cs
@@ -51,43 +51,41 @@ namespace MUIVPremiumAdminTool
             // 
             // tabControl
             // 
-            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl.Controls.Add(this.ADSearchPage);
             this.tabControl.Controls.Add(this.HRDepartmentPage);
             this.tabControl.Controls.Add(this.InformationPage);
             this.tabControl.Controls.Add(this.SettingsPage);
-            this.tabControl.Location = new System.Drawing.Point(13, 12);
+            this.tabControl.Location = new System.Drawing.Point(-4, -1);
+            this.tabControl.Margin = new System.Windows.Forms.Padding(0);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(460, 620);
+            this.tabControl.Size = new System.Drawing.Size(493, 669);
             this.tabControl.TabIndex = 0;
             this.tabControl.Click += new System.EventHandler(this.tabControl_Click);
             this.tabControl.Enter += new System.EventHandler(this.tabControl_Enter);
             // 
             // ADSearchPage
             // 
-            this.ADSearchPage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ADSearchPage.ForeColor = System.Drawing.Color.Black;
+            this.ADSearchPage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ADSearchPage.ForeColor = System.Drawing.Color.White;
             this.ADSearchPage.Location = new System.Drawing.Point(4, 22);
             this.ADSearchPage.Name = "ADSearchPage";
             this.ADSearchPage.Padding = new System.Windows.Forms.Padding(3);
-            this.ADSearchPage.Size = new System.Drawing.Size(452, 594);
+            this.ADSearchPage.Size = new System.Drawing.Size(485, 643);
             this.ADSearchPage.TabIndex = 0;
             this.ADSearchPage.Text = "ADский Поисковик";
             // 
             // HRDepartmentPage
             // 
+            this.HRDepartmentPage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.HRDepartmentPage.Controls.Add(this.HRDepartamentControl);
             this.HRDepartmentPage.ForeColor = System.Drawing.Color.White;
             this.HRDepartmentPage.Location = new System.Drawing.Point(4, 22);
             this.HRDepartmentPage.Name = "HRDepartmentPage";
             this.HRDepartmentPage.Padding = new System.Windows.Forms.Padding(3);
-            this.HRDepartmentPage.Size = new System.Drawing.Size(452, 594);
+            this.HRDepartmentPage.Size = new System.Drawing.Size(485, 643);
             this.HRDepartmentPage.TabIndex = 1;
             this.HRDepartmentPage.Text = "Отдел кадров";
-            this.HRDepartmentPage.UseVisualStyleBackColor = true;
             // 
             // HRDepartamentControl
             // 
@@ -96,53 +94,53 @@ namespace MUIVPremiumAdminTool
             | System.Windows.Forms.AnchorStyles.Right)));
             this.HRDepartamentControl.Controls.Add(this.GeneralUniversityPage);
             this.HRDepartamentControl.Controls.Add(this.BranchesUniversityPage);
-            this.HRDepartamentControl.Location = new System.Drawing.Point(6, 6);
+            this.HRDepartamentControl.Location = new System.Drawing.Point(-4, 0);
             this.HRDepartamentControl.Name = "HRDepartamentControl";
             this.HRDepartamentControl.SelectedIndex = 0;
-            this.HRDepartamentControl.Size = new System.Drawing.Size(440, 582);
+            this.HRDepartamentControl.Size = new System.Drawing.Size(496, 647);
             this.HRDepartamentControl.TabIndex = 0;
             this.HRDepartamentControl.Click += new System.EventHandler(this.HRDepartamentControl_Click);
             this.HRDepartamentControl.Enter += new System.EventHandler(this.HRDepartamentControl_Enter);
             // 
             // GeneralUniversityPage
             // 
+            this.GeneralUniversityPage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.GeneralUniversityPage.Location = new System.Drawing.Point(4, 22);
             this.GeneralUniversityPage.Name = "GeneralUniversityPage";
             this.GeneralUniversityPage.Padding = new System.Windows.Forms.Padding(3);
-            this.GeneralUniversityPage.Size = new System.Drawing.Size(432, 556);
+            this.GeneralUniversityPage.Size = new System.Drawing.Size(488, 621);
             this.GeneralUniversityPage.TabIndex = 0;
             this.GeneralUniversityPage.Text = "Москвичи";
-            this.GeneralUniversityPage.UseVisualStyleBackColor = true;
             // 
             // BranchesUniversityPage
             // 
+            this.BranchesUniversityPage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.BranchesUniversityPage.Location = new System.Drawing.Point(4, 22);
             this.BranchesUniversityPage.Name = "BranchesUniversityPage";
             this.BranchesUniversityPage.Padding = new System.Windows.Forms.Padding(3);
-            this.BranchesUniversityPage.Size = new System.Drawing.Size(432, 556);
+            this.BranchesUniversityPage.Size = new System.Drawing.Size(488, 621);
             this.BranchesUniversityPage.TabIndex = 1;
             this.BranchesUniversityPage.Text = "ЗаМКАДыши";
-            this.BranchesUniversityPage.UseVisualStyleBackColor = true;
             // 
             // InformationPage
             // 
+            this.InformationPage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.InformationPage.Location = new System.Drawing.Point(4, 22);
             this.InformationPage.Name = "InformationPage";
             this.InformationPage.Padding = new System.Windows.Forms.Padding(3);
-            this.InformationPage.Size = new System.Drawing.Size(452, 594);
+            this.InformationPage.Size = new System.Drawing.Size(485, 643);
             this.InformationPage.TabIndex = 2;
             this.InformationPage.Text = "Реклама";
-            this.InformationPage.UseVisualStyleBackColor = true;
             // 
             // SettingsPage
             // 
+            this.SettingsPage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.SettingsPage.Location = new System.Drawing.Point(4, 22);
             this.SettingsPage.Name = "SettingsPage";
             this.SettingsPage.Padding = new System.Windows.Forms.Padding(3);
-            this.SettingsPage.Size = new System.Drawing.Size(452, 594);
+            this.SettingsPage.Size = new System.Drawing.Size(485, 643);
             this.SettingsPage.TabIndex = 3;
             this.SettingsPage.Text = "Сетинги";
-            this.SettingsPage.UseVisualStyleBackColor = true;
             // 
             // notifyIcon
             // 
@@ -170,7 +168,7 @@ namespace MUIVPremiumAdminTool
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(484, 661);
             this.Controls.Add(this.tabControl);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
--- MUIVPremiumAdminTool/RegisterForm.Designer.cs
+++ MUIVPremiumAdminTool/RegisterForm.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(484, 261);
             this.MaximumSize = new System.Drawing.Size(500, 300);
             this.MinimumSize = new System.Drawing.Size(500, 300);
